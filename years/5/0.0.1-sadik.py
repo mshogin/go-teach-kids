cf51ed4ba7fd5a0b1f45e53c7e3b73bfed93c556
Mikhail Shogin <mshogin@gmail.com>
2020-03-23 23:27:53 +0100

    Add shell suggestions

--- years/5/0.0.1-sadik.py
+++ years/5/0.0.1-sadik.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
-
+# shell:
+# $ python 0.0.1-sadik.py     : wow
+# $ repeat 100 0.0.1-sadik.py : fun
 print("ПАПА + ТАСЯ = САДИК")
                                                                                                                                                                                                                           ;[pdpep-e=4'd
                                                                                                                                                                                                                           ;
                                                                                                                                                                                                                           s\d;[\ss
                                                                                                                                                                                                                           ]e]e\
+
