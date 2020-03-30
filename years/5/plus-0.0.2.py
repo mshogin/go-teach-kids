70716ecfa77482c3d6c2eb463e44669a82110cb1
Mikhail Shogin <mshogin@gmail.com>
2020-03-31 00:13:11 +0200

    plus-0.0.2.py

---
+++ years/5/plus-0.0.2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# shell:
+# $ python plus-0.0.2.py     : wow
+# $ sadik-0.0.2.py : fun
+# --
+# First type:
+print 1 + 1
+# then open terminal and run the command
+# and next iteration.
+print 1 + 2
+print 1 + 3
+print 1 + 4
+# The goal is to show to the kid that you can change something in one place
+# and you will get change in another place.
+# On each step you need to discuss with the child that line
+# "print 1 + x". Find the resolution like 1 finger + 2 fingers = 3 fingers.
+# Requirements:
+# Keys "Backspace" and "Enter" should be known before (you can involve the kid to type that program)
+# Side effect:
+# Getting known operation Save File, switch to terminal, re-run previous command.
+
---
+++ years/5/sadik-0.0.1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# shell:
+# $ python sadik-0.0.1.py     : wow
+# $ repeat 100 sadik-0.0.1.py : fun
+
+print("ПАПА + ТАСЯ = САДИК")
+                                                                                                                                                                                                                          ;[pdpep-e=4'd
+                                                                                                                                                                                                                          ;
+                                                                                                                                                                                                                          s\d;[\ss
+                                                                                                                                                                                                                          ]e]e\
+
